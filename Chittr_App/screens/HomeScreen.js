9cdfcb065f2e18c0e0f298128ff65afa94424334
Arman <arman1apollon@gmail.com>
2020-03-09 20:20:11 +0000

    Implemented GET/chits at HomeScreen

--- Chittr_App/screens/HomeScreen.js
+++ Chittr_App/screens/HomeScreen.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react';
-import { Text, View,Button } from 'react-native';
+import { Text, View, ActivityIndicator,ScrollView,FlatList } from 'react-native';
+
 
 
    
 class HomeScreen extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            chitsListData: [],
+            isLoading: true
+        }
+    }
+    
     static navigationOptions = {
         header: null
        }
 
+    getChits = () => {
+        return fetch('http://10.0.2.2:3333/api/v0.0.5/chits')
+      .then((response) => response.json())
+      .then((responseJson) => {
+      this.setState({
+      isLoading: false,
+      chitsListData: responseJson,
+      });
+      console.log(responseJson)
+      }).catch((error) =>{
+      console.log(error);
+      });
+    }
+    componentDidMount(){
+        this.getChits();
+       }
+
  render(){
- return(
- <View>
- <Text>Home Screen</Text>
- </View>
- );
- }
-}
+    if(this.state.isLoading){
+        return(
+        <View>
+        <ActivityIndicator/>
+        </View>
+        )
+        }
+    return(
+        <View>
+            
+        <FlatList
+            data={this.state.chitsListData}
+            renderItem={({item}) =>( 
+                <ScrollView>
+                <Text>{item.chit_content} </Text>
+                </ScrollView>
+            )}
+        keyExtractor={({id}, index) => id}
+        
+        />
+            
+            
+        </View>
+    );
+    }
+    }
 export default HomeScreen;
