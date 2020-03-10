239955c9aa333a45f0288fef72ec16542cdf5985
Arman <arman1apollon@gmail.com>
2020-03-10 01:01:20 +0000

    GET/ endpoint at homescreen implemented

--- Chittr_App/App.js
+++ Chittr_App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet,Text, View,TextInput, Button, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import HomeScreen from './screens/HomeScreen'
@@ -9,120 +9,146 @@ import FPanelScreen from './screens/FPanelScreen'
 
 
 
-class App extends Component{
+class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-     this.state={
-       email: '',
-       password: '',
-       authCode: ''
-     }
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+      surname: '',
+      authCode: ''
     }
-   
-   Login = () => {
-     console.log(this.state.email + "  " + this.state.password);
+  }
 
-     return fetch("http://10.0.2.2:3333/api/v0.0.5/login",
-        {
+  Login = () => {
+    console.log(this.state.email + "  " + this.state.password);
+
+    return fetch("http://10.0.2.2:3333/api/v0.0.5/login",
+      {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
 
         body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password
-        
-        })
-        })
-        .then((response) => response.json()).then((responseJson) => {
-          this.setState({authCode : responseJson.token});
-          console.log(responseJson)
-        }).catch((error) => {
-        console.error(error);
-        });
-   }
-   
-    render() {
-    return (
-      <View style= {styles.container}>
-        <Text style = {styles.title}>Chittr</Text>
-     <TextInput
-       style={styles.textInput}
-       placeholder = "Email"
-       onChangeText={(text) => this.setState({email: text})} 
-       value = {this.state.email}
-     />
-     <TextInput 
-       style={styles.textInput}
-       placeholder = "Password"
-       onChangeText={(text) => this.setState({password: text})} 
-       value = {this.state.password}
-       secureTextEntry={true}
-     />
-     <View style={styles.btnContainer}>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={this.Login}
-      >
-        <Text style={styles.btnText}>Log In</Text>
-      </TouchableOpacity>
+          email: this.state.email,
+          password: this.state.password
 
-      <TouchableOpacity
-        style={styles.btn}
-      >
-        <Text style={styles.btnText}>Register</Text>
-      </TouchableOpacity>
-     </View>
-   </View>
-     );
-    }
-   }
-   
-   const styles = StyleSheet.create({
-     container : {
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: '#8BD5C7'
-     },
-    title : { 
-       fontSize: 50,   
-       marginBottom: 150,
-       marginTop: 100
-     },
-     textInput : {
-       width: "90%",
-       marginBottom: 10,
-       backgroundColor: "#fff",
-       padding: 15
-     },
-     btnContainer : {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        width: "90%"
-     },
-     btn : {
-       backgroundColor: "#3AA18D",
-       padding: 15,
-       width: "45%"
-     },
-     btnText: {
-       fontSize:18,
-       textAlign: "center",
-     } 
-    
-    
-   });
+        })
+      })
+      .then((response) => response.json()).then((responseJson) => {
+        this.setState({ authCode: responseJson.token });
+        console.log(responseJson)
+      }).catch((error) => {
+        console.error(error);
+      });
+  }
+
+  Register = () => {
+    return fetch("http://10.0.2.2:3333/api/v0.0.5/user",
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+          name: this.state.name,
+          surname: this.state.surname
+        })
+      })
+      .then((response) => response.json()).then((responseJson) => {
+        this.setState({ authCode: responseJson.token });
+        console.log(responseJson)
+      }).catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Chittr</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email"
+          onChangeText={(text) => this.setState({ email: text })}
+          value={this.state.email}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          onChangeText={(text) => this.setState({ password: text })}
+          value={this.state.password}
+          secureTextEntry={true}
+        />
+        <View style={styles.btnContainer}>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={this.Login}
+          >
+            <Text style={styles.btnText}>Log In</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.btn}
+
+          >
+            <Text style={styles.btnText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: '#8BD5C7'
+  },
+  title: {
+    fontSize: 50,
+    marginBottom: 150,
+    marginTop: 100
+  },
+  textInput: {
+    width: "90%",
+    marginBottom: 10,
+    backgroundColor: "#fff",
+    padding: 15
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%"
+  },
+  btn: {
+    backgroundColor: "#3AA18D",
+    padding: 15,
+    width: "45%"
+  },
+  btnText: {
+    fontSize: 18,
+    textAlign: "center",
+  }
+
+
+});
 
 
 const AppTabNav = createBottomTabNavigator({
-  
+
   Home: {
-  screen: HomeScreen
+    screen: HomeScreen
   },
   Post: {
-  screen: PostScreen
+    screen: PostScreen
   },
   Profile: {
     screen: ProfileScreen
@@ -130,8 +156,8 @@ const AppTabNav = createBottomTabNavigator({
   FPanel: {
     screen: FPanelScreen
   }
- });
- 
- const AppContainer = createAppContainer(AppTabNav)
+});
 
- export default AppContainer;
+const AppContainer = createAppContainer(AppTabNav)
+
+export default App;
--- Chittr_App/screens/HomeScreen.js
+++ Chittr_App/screens/HomeScreen.js
@@ -1,64 +1,101 @@
 import React, { Component } from 'react';
-import { Text, View, ActivityIndicator,ScrollView,FlatList } from 'react-native';
+import { Text, View, ActivityIndicator, ScrollView, FlatList, StyleSheet } from 'react-native';
 
-
-
+const styles = StyleSheet.create({
+    container : {
+       flex: 1,
+       alignItems: "center",
+       backgroundColor: '#54CBFF'
+    },
+   title : { 
+      fontSize: 50,   
+      marginBottom: 150,
+      marginTop: 100
+    },
+    textName : {
+      fontSize: 18,
+      padding: 20,
+      paddingLeft: 0,
+      fontWeight: "bold" 
+    },
+    ScrollViewContainer : {
+       width: "90%",
+       padding: 20
+       
+    },
+    viewFlatList : {
+        flexDirection: "column",
+        justifyContent: "center",
+        width: "90%",
+    },
+    textChit: {
+      fontSize:16,
+      paddingBottom: 10,
+      paddingLeft:10,
+      backgroundColor: "#38B0DE"
+    } 
    
-class HomeScreen extends Component{
-    constructor(props){
+   
+  });
+
+class HomeScreen extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             chitsListData: [],
             isLoading: true
         }
     }
-    
+
     static navigationOptions = {
         header: null
-       }
+    }
 
     getChits = () => {
         return fetch('http://10.0.2.2:3333/api/v0.0.5/chits')
-      .then((response) => response.json())
-      .then((responseJson) => {
-      this.setState({
-      isLoading: false,
-      chitsListData: responseJson,
-      });
-      console.log(responseJson)
-      }).catch((error) =>{
-      console.log(error);
-      });
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    chitsListData: responseJson,
+                });
+                console.log(responseJson)
+            }).catch((error) => {
+                console.log(error);
+            });
     }
-    componentDidMount(){
+    componentDidMount() {
         this.getChits();
-       }
+    }
 
- render(){
-    if(this.state.isLoading){
-        return(
-        <View>
-        <ActivityIndicator/>
-        </View>
-        )
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <View>
+                    <ActivityIndicator />
+                </View>
+            )
         }
-    return(
-        <View>
-            
-        <FlatList
-            data={this.state.chitsListData}
-            renderItem={({item}) =>( 
-                <ScrollView>
-                <Text>{item.chit_content} </Text>
+        return (
+            <View style={styles.container}>
+                <ScrollView style={styles.ScrollViewContainer}>
+                <FlatList
+                    data={this.state.chitsListData}
+                    renderItem={({ item }) => (
+                        <View style={styles.viewFlatList}>
+                            <Text style={styles.textName}>{item.user.given_name}: </Text>
+                            <Text style={styles.textChit}>{item.chit_content}</Text>
+                        </View>
+                    )}
+                    keyExtractor={item => item.chit_id}
+
+                />
                 </ScrollView>
-            )}
-        keyExtractor={({id}, index) => id}
-        
-        />
-            
-            
-        </View>
-    );
+
+            </View>
+        );
     }
-    }
+}
+
+
 export default HomeScreen;
